3ca7a011e5c72be14074fff64ccae491631f307b
Tiryoh <tiryoh@gmail.com>
2016-11-09 13:49:34 +0900

    Add inputrc installation to Makefile

--- etc/init/backup_bash.sh
+++ etc/init/backup_bash.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+if [ -a ${HOME}/.inputrc ]; then mv ${HOME}/.inputrc ${HOME}/.inputrc.bak; fi;
 if [ -a ${HOME}/.bashrc ]; then mv ${HOME}/.bashrc ${HOME}/.bashrc.bak; fi;
 if [ -a ${HOME}/.bash_profile ]; then mv ${HOME}/.bash_profile ${HOME}/.bash_profile.bak; fi;
